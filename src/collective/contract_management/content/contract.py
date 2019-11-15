23c72a3f4dbe12abcb935ad7ec41b75589329bce
MrTango <md@derico.de>
2019-11-15 15:04:46 +0100

    Add content type schema

--- src/collective/contract_management/content/contract.py
+++ src/collective/contract_management/content/contract.py
@@ -6,55 +6,66 @@ from plone.dexterity.content import Container
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
-# from collective.contract_management import _
+from collective.contract_management import _
 
 
 class IContract(model.Schema):
     """ Marker interface and Dexterity Python Schema for Contract
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('contract.xml')
+    begin = schema.Date(
+       title=_(
+           u'Contract begin',
+       ),
+       description=_(
+           u'',
+       ),
+       # defaultFactory=get_default_contract_begin,
+       required=True,
+    )
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    end = schema.Date(
+       title=_(
+           u'Contract end',
+       ),
+       description=_(
+           u'',
+       ),
+       # defaultFactory=get_default_contract_end,
+       required=True,
+    )
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    notice_period = schema.Date(
+       title=_(
+           u'Notice period',
+       ),
+       description=_(
+           u'',
+       ),
+       # defaultFactory=get_default_contract_end,
+       required=False,
+    )
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+    # Make sure to import: plone.app.vocabularies as vocabs
+    reminder = schema.Choice(
+        title=_(
+            u'Reminder',
+        ),
+        description=_(
+            u'Reminder, in days before Notice period.',
+        ),
+        vocabulary=u'vocabs.PortalTypes',
+        default=u'',
+        # defaultFactory=get_default_reminder,
+        required=False,
+        readonly=False,
+    )
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
 
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
 
 
 @implementer(IContract)
