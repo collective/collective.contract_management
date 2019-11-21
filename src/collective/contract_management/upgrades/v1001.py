3930691ceffb43de31fed764d10cfe8fc8275b01
MrTango <md@derico.de>
2019-11-21 12:47:52 +0100

    fix upgrade step

--- src/collective/contract_management/upgrades/v1001.py
+++ src/collective/contract_management/upgrades/v1001.py
@@ -2,13 +2,16 @@
 
 from . import logger
 from plone import api
+from base import reload_gs_profile
 
 
 def upgrade(setup_tool=None):
     """
     """
     logger.info("Running upgrade (Python): Enable Notice period criteria for Collections")
+    reload_gs_profile(setup_tool)
     results = api.content.find(portal_type='Contract')
+    import pdb; pdb.set_trace()
     for brain in results:
         obj = brain.getObject()
         obj.reindexObject()
