0b533ba3e76a7ca42410c4b1e5fc49f82ec9df6c
MrTango <md@derico.de>
2019-11-21 12:22:30 +0100

    Add upgrade step

---
+++ src/collective/contract_management/upgrades/__init__.py
@@ -0,0 +1,4 @@
+import logging
+
+
+logger = logging.getLogger("collective.contract_management")
--- src/collective/contract_management/upgrades/v1001.py
+++ src/collective/contract_management/upgrades/v1001.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from . import logger
+from plone import api
 
 
 def upgrade(setup_tool=None):
     """
     """
     logger.info("Running upgrade (Python): Enable Notice period criteria for Collections")
+    results = api.content.find(portal_type='Contract')
+    for brain in results:
+        obj = brain.getObject()
+        obj.reindexObject()
+        logger.info("Reindex: {}".format(obj.absolute_url()))
