8dda6e8c99555c92b9649414f1f3ba08cbac7a30
MrTango <md@derico.de>
2019-11-21 12:05:58 +0100

    "Add upgrade_step: Enable Notice period criteria for Collections"

---
+++ src/collective/contract_management/upgrades/v1001.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import logger
+
+
+def upgrade(setup_tool=None):
+    """
+    """
+    logger.info("Running upgrade (Python): Enable Notice period criteria for Collections")
